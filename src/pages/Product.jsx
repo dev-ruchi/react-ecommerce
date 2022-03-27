a75fcc1cefed0f78b5b083e0b012f9a2a380b833
Ruchi <ruchi.thanabhawan@gmail.com>
2022-03-27 14:37:06 +0530

    load product state using slug

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
+import products from '../data/products'
 
 function Product() {
-
-
     const { slug } = useParams()
+    const [product, setProduct] = useState(getProuctFromSlug())
+
+    function getProuctFromSlug() {
+       return products.find(item => item.slug === slug)
+    }
+
+
 
     return (
-        <Container>{ slug }</Container>
+        <Container>
+            
+        </Container>
     )
 }
 
