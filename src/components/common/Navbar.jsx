0ea330e613d2256a586ae6166b48e3271c14856c
Ruchi <ruchi.thanabhawan@gmail.com>
2022-04-02 09:43:22 +0530

    add link to cart page

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -52,13 +52,13 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
+                <Link
+                  to="/cart"
                   className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 >
                   <span className="sr-only">View notifications</span>
                   <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+                </Link>
               </div>
             </div>
           </div>
